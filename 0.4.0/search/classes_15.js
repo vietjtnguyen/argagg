5071fc0a7a397b2bfdf4ff30ef22d6757de7c56e
Viet T. Nguyen <vietjtnguyen@gmail.com>
2017-03-07 01:48:02 -0800

    Added 0.4.0 documentation

---
+++ 0.4.0/search/classes_15.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['va_5flist',['va_list',['http://en.cppreference.com/w/cpp/utility/variadic/va_list.html',1,'']]],
+  ['valarray',['valarray',['http://en.cppreference.com/w/cpp/numeric/valarray.html',1,'std']]],
+  ['value_5fcompare',['value_compare',['http://en.cppreference.com/w/cpp/container/map/value_compare.html',1,'std::map&lt; K, T &gt;::value_compare'],['http://en.cppreference.com/w/cpp/container/multimap/value_compare.html',1,'std::multimap&lt; K, T &gt;::value_compare']]],
+  ['vector',['vector',['http://en.cppreference.com/w/cpp/container/vector.html',1,'std']]],
+  ['vector_3c_20argagg_3a_3adefinition_20_3e',['vector&lt; argagg::definition &gt;',['http://en.cppreference.com/w/cpp/container/vector.html',1,'std']]],
+  ['vector_3c_20argagg_3a_3aoption_5fresult_20_3e',['vector&lt; argagg::option_result &gt;',['http://en.cppreference.com/w/cpp/container/vector.html',1,'std']]],
+  ['vector_3c_20const_20char_20_2a_3e',['vector&lt; const char *&gt;',['http://en.cppreference.com/w/cpp/container/vector.html',1,'std']]],
+  ['vector_3c_20std_3a_3astring_20_3e',['vector&lt; std::string &gt;',['http://en.cppreference.com/w/cpp/container/vector.html',1,'std']]]
+];
---
+++ 0.4.0/search/variables_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['options',['options',['../structargagg_1_1parser__results.html#ae1aa657b375b7e4b5c918fa08ff402b7',1,'argagg::parser_results']]]
+];
